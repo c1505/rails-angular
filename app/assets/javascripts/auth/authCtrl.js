a0846c2ce701714e47820fdedabfb0a740e2283a
c1505 <coreym247@gmail.com>
2016-09-02 06:54:31 -0400

    registration working through web form

--- app/assets/javascripts/auth/authCtrl.js
+++ app/assets/javascripts/auth/authCtrl.js
@@ -11,7 +11,7 @@ function($scope, $state, Auth){
   };
   $scope.register = function() {
     Auth.register($scope.user).then(function(){
-      $state.go('home');
+      $state.go('tickets');
     });
   };
 }]);
